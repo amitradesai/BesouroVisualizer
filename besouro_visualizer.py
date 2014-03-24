f5f79b81e2c83abde62afdbfe2f4d27d8eae2d83
dfucci <davide.fucci@oulu.fi>
2014-03-24 13:57:07 +0200

    added parsing line

--- besouro_visualizer.py
+++ besouro_visualizer.py
@@ -1,6 +1,16 @@
 from episode import Episode
+import re
+def import_from_log(file):
+	
+def parse_line_episode(line):
+	line_array = re.split('\s+', line)
+	timestamp = int(line_array[0]) // 1000
+	type = line_array[1]
+	ep = Episode(timestamp, type)
 def main():
 	ep1  = Episode(123, "TDD")
 	ep2  = Episode(122, "TDD")
-	ep1_duration = ep1.duration(ep2)
-	print ep1_duration
+	ep1.calculate_duration(ep2)
+	print ep1.duration
+
+if  __name__ =='__main__':main()
