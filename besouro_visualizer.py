45e4b32d82208d91251b4485c3a257d53bf11c87
dfucci <davide.fucci@oulu.fi>
2014-04-09 13:04:14 +0300

    refactorings

--- besouro_visualizer.py
+++ besouro_visualizer.py
@@ -2,12 +2,14 @@ from episode import Episode
 import re
 import csv
 import sys
+
+DIVIDER_FOR_STANDARD_TIMESTAMP_FORMAT = 1000
 def import_from_log(episode_file):
 	return [parse_line_episode(line.strip()) for line in open(episode_file)]
 
 def parse_line_episode(line):
 	line_array = re.split('\s+', line)
-	timestamp = int(line_array[0]) // 1000
+	timestamp = sanitize_timestamp(line_array[0])
 	type = line_array[1]
 	return  Episode(timestamp, type)
 
@@ -18,9 +20,12 @@ def calculate_durations(episodes):
 	return episodes
 
 def write_to_csv(output_file, episodes):
-	with open(output_file, "wb") as file:
+	with open(output_file, "wb") as f:
 		for episode in episodes:
-			output_file.write("{0}; {1} \n".format(episode.type, episode.duration))
+			f.write("{0}; {1} \n".format(episode.type, episode.duration))
+
+def sanitize_timestamp(timestamp):
+	return int(timestamp) // DIVIDER_FOR_STANDARD_TIMESTAMP_FORMAT
 
 def main(argv):
 	output_file = argv.split("\\")[4] + ".csv"
